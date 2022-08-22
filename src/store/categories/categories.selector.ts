0921d3fd675e0492b7fcd744c02e24e526f6a42c
SamariddinS <ssamar.96.96@gmail.com>
2022-08-22 22:39:24 +0500

    rewritten to TypeScript

--- src/store/categories/categories.selector.ts
+++ src/store/categories/categories.selector.ts
@@ -1,6 +1,9 @@
 import { createSelector } from 'reselect';
 
-const selectCategoryReducer = (state) => state.categories;
+import { CategoriesState } from './categories.reducer';
+import { CategoryMap } from './categories.types';
+
+const selectCategoryReducer = (state: ): CategoriesState => state.categories;
 
 export const selectCategories = createSelector(
   [selectCategoryReducer],
@@ -9,12 +12,12 @@ export const selectCategories = createSelector(
 
 export const selectCategoriesMap = createSelector(
   [selectCategories],
-  (categories) =>
+  (categories): CategoryMap =>
     categories.reduce((acc, category) => {
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
-    }, {})
+    }, {} as CategoryMap)
 );
 
 export const selectIsLoading = createSelector(
